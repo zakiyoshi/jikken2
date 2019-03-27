48784e4f144481d3513e901ddb49baa5a84d6a11
Zakiyoshi Jouichi <zakiyoshi.jouichi@gmail.com>
2019-03-27 16:54:00 +0900

    first commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :jikken1 do
+  name 'Jikken1 plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.5.4'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
