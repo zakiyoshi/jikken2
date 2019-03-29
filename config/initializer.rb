727dde8a25d6a6974b5581900348b0691660dbf7
yoshizaki <zakiyoshi.jouichi@gmail.com>
2019-03-29 16:39:51 +0900

    8.0.0

--- config/initializer.rb
+++ config/initializer.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :jikken1 do
   name 'Jikken2 plugin'
   author 'Author name'
   description 'This is a plugin for Redmine'
-  version '7.0.2'
+  version '8.0.0'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
 end
